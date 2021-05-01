3df53dccc471ee7e9514d22d6ca220c8f713734b
Kevin Booth <kevin@kb.gg>
2021-05-01 12:04:26 -0500

    Workflow Testing

---
+++ scripts/compile.py
@@ -0,0 +1,20 @@
+import glob
+import json
+
+def main():
+    identifiers = {}    
+    identifier_files = glob.glob('identifiers/*.json')
+
+    for identifier_file in identifier_files:
+        if identifier_file == 'identifiers/template.json':
+            continue
+
+        with open(identifier_file, 'r') as f:
+            data = json.load(f)
+            identifiers[data['id']] = data
+
+    with open('build/identifiers.json', 'w') as f:
+        f.write(json.dumps(identifiers, indent=4, sort_keys=True))
+
+if __name__ == '__main__':
+    main()
